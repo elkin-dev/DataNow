4ed16f76a54d966bce2b31e72e4364094d30140a
elkin <elkin.dev22@gmail.com>
2024-03-07 11:25:03 -0500

    Solucion 2

---
+++ sql/table_master_data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE
+  TBL_DATOS_AGENTE (
+    Id SERIAL PRIMARY KEY,
+    Nombre_Agente VARCHAR (100),
+    Agente_Ofei VARCHAR (100),
+    Central VARCHAR (100),
+    Total_Horas FLOAT
+  );
